b2347d5f3c8f4d53e8b5bf1bac9ee307732751b7
João Giordani <jgiordanii@gmail.com>
2022-12-01 19:08:51 -0300

    final commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var button = document.getElementById('read_btn');
+
+button.addEventListener('click', function() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        return button.textContent = 'Read Less';
+    }
+    button.textContent = 'Read More';
+});
